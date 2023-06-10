cfb8bab2875db44aa111bf373c9fda0369abdcc1
Ivan Pereira <ivanpereira@MacBook-Pro-de-Ivan.local>
2023-06-10 16:21:35 -0400

    no using components deleted

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,8 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
-
-// import MenuIcon from "@mui/icons-material/Menu";
 
 export default function NavBar() {
 	return (
